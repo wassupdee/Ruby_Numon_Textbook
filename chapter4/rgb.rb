15d28854ca179dd7067b025e1d5afd71fd8eb93f
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-04 08:38:30 +0900

    練習問題

---
+++ chapter4/rgb.rb
@@ -0,0 +1,10 @@
+def to_hex(r, g, b)
+  [r, g, b].sum('#') do |n|
+    n.to_s(16).rjust(2,'0')
+  end
+end
+
+def to_ints(hex)
+  r, g, b = hex[1..2], hex[3..4], hex[5..6]
+  [r, g, b].map(&:hex)
+end
