f01295089b49148f534d5548d32b376a55d508e9
wassupdee <daichi.hirohata.japan@gmail.com>
2024-07-14 10:05:53 +0900

    クラスメソッドを使う目的

--- chapter7/chapter7.rb
+++ chapter7/chapter7.rb
@@ -14,3 +14,30 @@ end
 @user.first_name #=> error クラス外からインスタンス変数は呼び出せないため。
 # ただし、attr_accessor :first_nameをつけると、クラスからインスタンス変数を呼び出すメソッドが生成される。このメソッドで、インスタンス変数を呼び出せる
 @user.first_name #=> "Alice"
+
+24/7/14
+1. クラスメソッドは、つくられた個々のインスタンスをまとめた情報を管理する（例：つくられたインスタンスの個数などカウントする）
+2. インスタンスメソッドは、それ自身の情報しか見ていない
+
+class User
+  @@count = 0
+
+  def initialize
+   @@count += 1
+  end
+
+  def self.users_count
+    puts "現在のユーザー数は#{@@count}人です"
+  end
+end
+
+#Userクラスから、３つのインスタンスが作成される
+#この時に、Userクラスのcountが１つずつ上がる
+User.new
+User.new
+User.new
+
+#Userクラス自身がカウントしていた数を出力する
+User.users_count
+
+=> 現在のユーザー数は3人です
